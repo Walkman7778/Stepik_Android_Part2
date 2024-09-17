72f47ed90ee730522e8c5b67b4865c2e209f3f91
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-09-17 23:29:16 +0200

    Creating app Messenger

--- MyApp03Messenger/app/src/main/java/com/example/myapp03messenger/MainActivity.java
+++ MyApp03Messenger/app/src/main/java/com/example/myapp03messenger/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapp03messenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        EditText editTextMessage = findViewById(R.id.editTextTextPersonName);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editTextMessage.getText().toString();
+                // in the buttonClickListener we call function lauchNextScreen which has as parameter
+                // string variable containing text from first layout editText container
+                launchNextScreen(message);
+            }
+        });
+    }
+    private void launchNextScreen(String message){
+        // here intent is using for opening new activity which has as parameters
+        // context and activity which must be opened with resolution class -it is an object
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        // we put in intent variable message with key "message" and transmit it to the second activity
+        intent.putExtra("message", message);
+        // starting second activity
+        startActivity(intent);
+
+
     }
 }
--- MyApp03Messenger/app/src/main/java/com/example/myapp03messenger/ReceivedMessageActivity.java
+++ MyApp03Messenger/app/src/main/java/com/example/myapp03messenger/ReceivedMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapp03messenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ReceivedMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_message);
+        // getting intent from  first activity with its content
+        Intent intent = getIntent();
+        // creating a string variable in which we put value of intent variable with key "message"
+        String message = intent.getStringExtra("message");
+        TextView textView = findViewById(R.id.textV2ReceivedMessage);
+        textView.setText(message);
     }
 }
