26d45a064daaf15d0a2c57cd7347a1c8c743815f
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-09-17 02:28:00 +0200

    creating logic for simple app creating listener for button

--- app/src/main/java/com/example/myapp01/MainActivity.java
+++ app/src/main/java/com/example/myapp01/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.myapp01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.PlainTextSampleAnswer);
+        Button button = findViewById(R.id.button);
+        TextView textViewCorrectAnsw = findViewById(R.id.correctView);
+        TextView textViewInCorrectAnsw = findViewById(R.id.incorrectView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                int number = Integer.parseInt(text);
+                if (number == 12){
+                    textViewCorrectAnsw.setVisibility(View.VISIBLE);
+                    textViewInCorrectAnsw.setVisibility(View.GONE);
+
+                } else{
+                    textViewInCorrectAnsw.setVisibility(View.VISIBLE);
+                    textViewCorrectAnsw.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 }
-// creating a sample   of  commit  to  verify project name
