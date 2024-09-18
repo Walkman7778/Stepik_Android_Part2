ddca1da8ebe868ae9a05847f97d4af7b6a4886a3
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-09-18 22:39:27 +0200

    life cycle on stop, on start, working with logs

--- MyApp04GameScor/app/src/main/java/com/example/myapp04gamescore/MainActivity.java
+++ MyApp04GameScor/app/src/main/java/com/example/myapp04gamescore/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,45 +13,65 @@ public class MainActivity extends AppCompatActivity {
 
     private int score1 = 0;
     private int score2 = 0;
+    private TextView textTeam1;
+    private TextView textTeam2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("MainActivity", "onCreate");
         // here we transmit to the variables score1 and score2 parameters from bundle by their key
         // only if it is not first activation of activity - first using till rotation the screen
         if (savedInstanceState != null) {
             score1 = savedInstanceState.getInt("score1");
             score2 = savedInstanceState.getInt("score2");
         }
-        TextView textTeam1 = findViewById(R.id.textCountTeam1);
-        TextView textTeam2 = findViewById(R.id.textCountTeam2);
 
-        textTeam1.setText(String.valueOf(score1));
-        textTeam2.setText(String.valueOf(score2));
+        textTeam1 = findViewById(R.id.textCountTeam1);
+        textTeam2 = findViewById(R.id.textCountTeam2);
+        score1();
+        score2();
 
         textTeam1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textTeam1.setText(String.valueOf(++score1));
+                score1++;
+                score1();
+
             }
         });
 
         textTeam2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textTeam2.setText(String.valueOf(++score2));
+                score2++;
+                score2();
+
             }
         });
 
 
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState){
         super.onSaveInstanceState(outState);
+        Log.d("MainActivity", "onSaveInstanceState");
         // here we transmit in object bundle values of variables score1 and score2 till rotating the screen
         outState.putInt("score1", score1);
         outState.putInt("score2", score2);
@@ -60,5 +81,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+    }
+
+
+
+
+
+
+
+
+
+    protected void score1(){
+        textTeam1.setText(String.valueOf(score1));
+    }
+
+
+    protected void score2(){
+        textTeam2.setText(String.valueOf(score2));
     }
 }
