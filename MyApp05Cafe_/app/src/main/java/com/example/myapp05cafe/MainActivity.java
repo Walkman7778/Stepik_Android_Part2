bc0bd6ac29e036472d642eb6d56825f3c48e79f1
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-09-22 00:05:07 +0200

    Creating logic for first activity, for app Caffe, working with intent , btn listener and condition for opening the second activity

--- MyApp05Cafe_/app/src/main/java/com/example/myapp05cafe/MainActivity.java
+++ MyApp05Cafe_/app/src/main/java/com/example/myapp05cafe/MainActivity.java
@@ -3,14 +3,57 @@ package com.example.myapp05cafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private EditText name;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, OrderDetilActivity.class);
+
+
+        initViews();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String verificationName = name.getText().toString().trim();
+                String verificationPassword = password.getText().toString().trim();
+                if ((verificationName.isEmpty()) | verificationPassword.isEmpty()){
+                    Toast.makeText(MainActivity.this,
+                            getString(R.string.error_fields_empty),
+                            // getString(R.string.error_fields_empty), here is the  same as
+                            // R.string.error_fields_empty
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    launchNextscreen(verificationName);
+                }
+
+
+            }
+        });
+    }
+
+
+    private void launchNextscreen(String verificationName){
+        Intent intent = MakeOrderActivity.newIntent(this, verificationName);
         startActivity(intent);
     }
+
+
+    private void initViews (){
+        btn = findViewById(R.id.signInButton);
+        name = findViewById(R.id.editTextName);
+        password = findViewById(R.id.editTextPassword);
+
+    }
+
 }
--- MyApp05Cafe_/app/src/main/java/com/example/myapp05cafe/MakeOrderActivity.java
+++ MyApp05Cafe_/app/src/main/java/com/example/myapp05cafe/MakeOrderActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapp05cafe;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MakeOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+    }
+
+
+    public static Intent newIntent(Context context, String verificationName){
+        Intent intent = new Intent(context, MakeOrderActivity.class);
+        intent.putExtra("name", verificationName);
+        return intent;
     }
 }
