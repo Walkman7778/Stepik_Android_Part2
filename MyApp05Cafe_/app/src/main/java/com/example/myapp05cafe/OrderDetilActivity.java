54e33cc2012946da55cccb8c49aeeae848ef4a9f
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-09-21 21:34:04 +0200

    Creating third layout, for app Caffe, working with scroll view group

--- MyApp05Cafe_/app/src/main/java/com/example/myapp05cafe/OrderDetilActivity.java
+++ MyApp05Cafe_/app/src/main/java/com/example/myapp05cafe/OrderDetilActivity.java
@@ -1,16 +1,13 @@
 package com.example.myapp05cafe;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OrderDetilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, OrderDetilActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_order_detil);
     }
 }
