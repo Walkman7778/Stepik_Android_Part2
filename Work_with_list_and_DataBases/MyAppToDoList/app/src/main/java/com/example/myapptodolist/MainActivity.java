6f98eee98d8a7dd4c5edd1f84120d01eb820ee4e
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-10-03 05:25:35 +0200

    Creating and realization of recycler view adapter, and using it. Changing in the class MainActivity variables linearLayout to the recyclerViewNotes from the NotesAdapter class

--- Work_with_list_and_DataBases/MyAppToDoList/app/src/main/java/com/example/myapptodolist/MainActivity.java
+++ Work_with_list_and_DataBases/MyAppToDoList/app/src/main/java/com/example/myapptodolist/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout linearLayoutsNotes;
+    private RecyclerView recyclerViewNotes;
     private FloatingActionButton buttonAddNotes;
 
     // creating array of notes by calling DatabaseNote.getInstance(); - the database from class
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews(){
-        linearLayoutsNotes = findViewById(R.id.linearLayoutsNotes);
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
         buttonAddNotes = findViewById(R.id.buttonAddNote);
     }
 
