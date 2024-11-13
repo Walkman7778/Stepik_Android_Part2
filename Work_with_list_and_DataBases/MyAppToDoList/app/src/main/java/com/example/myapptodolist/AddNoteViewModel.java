94df3f6062dc45e32d8209acef6f1fffe8cae49b
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-11-13 02:15:07 +0100

    Using RxJava3 library working with disposable and compositeDisposable arguments. also using method delay in RxJava3

--- Work_with_list_and_DataBases/MyAppToDoList/app/src/main/java/com/example/myapptodolist/AddNoteViewModel.java
+++ Work_with_list_and_DataBases/MyAppToDoList/app/src/main/java/com/example/myapptodolist/AddNoteViewModel.java
@@ -1,6 +1,7 @@
 package com.example.myapptodolist;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
@@ -8,8 +9,12 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -17,6 +22,7 @@ public class AddNoteViewModel extends AndroidViewModel {
 
     // here we subscribe to the notesDao directly.
     private NotesDao notesDao;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private MutableLiveData<Boolean> ShouldCloseScreen = new MutableLiveData<>();
     public AddNoteViewModel(@NonNull Application application) {
         super(application);
@@ -31,20 +37,29 @@ public class AddNoteViewModel extends AndroidViewModel {
         MutableLiveData ------ this action is  made  in  ontoher way by using RxJava3 libraries
         just switching the threads  is  easier */
     public void saveNote(Note note){
-        notesDao.add(note)
+        Disposable disposable = notesDao.add(note)
+                .delay(5, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action() {
             @Override
             public void run() throws Throwable {
-                ShouldCloseScreen.postValue(true);
+                Log.d("AddNoteViewModel","subscribe");
+                ShouldCloseScreen.setValue(true);
             }
         });
-
-
-
+        compositeDisposable.add(disposable);
     }
 
-
-
+    /**
+     * This method will be called when this ViewModel is no longer used and will be destroyed.
+     * <p>
+     * It is useful when ViewModel observes some data and you need to clear this subscription to
+     * prevent a leak of this ViewModel.
+     */
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
+    }
 }
