6c335eb66387b56578b3b981fa98cafa54b32f3f
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-11-22 20:04:44 +0100

    creating ViewModel and moving all logic of loading data into the viewModel

--- Stepik_Android_part3/app/src/main/java/com/example/dogs/MainActivity.java
+++ Stepik_Android_part3/app/src/main/java/com/example/dogs/MainActivity.java
@@ -1,63 +1,33 @@
 package com.example.dogs;
 
+import android.arch.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    // writing a url adress
-    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadDogImage();
-    }
-
-
-    private void loadDogImage() {
-        // here  as  usual working  with  intrnet and all  operations in internet must  have their
-        // own threads, so  for best  working is used new Thread
-        new Thread(() -> {
-            try {
-                URL url = new URL(BASE_URL);
-                HttpURLConnection uRLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = uRLConnection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String result;
-                StringBuilder data = new StringBuilder();
-
-                do {
-                    result = bufferedReader.readLine();
-                    if (result != null) {
-                        data.append(result);
-                    }
-                } while (result != null);
-                JSONObject jsonObject = new JSONObject(data.toString());
-                String message = jsonObject.getString("message");
-                String status = jsonObject.getString("status");
-                DogImage dogImage = new DogImage(message, status);
-
-
-                Log.d("MainActivity", dogImage.toString());
-
-            } catch (Exception e) {
-                Log.d("MainActivity", e.toString());
+        MainViewModel viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(MainViewModel.class);
+        viewModel.loadDogImage();
+        viewModel.getDogImage().observe(this, dogImage -> {
+            if (dogImage != null) {
+                Log.d(TAG, dogImage.toString());
             }
-        }).start();
+        });
+
+
+
 
     }
+
+
+
 
 }
